66b382cffc504e47573a6b716c681068af5fd113
x213212 <x213212@gmail.com>
2021-05-04 20:57:37 +0800

    add thread fucntion detecting and stmt  Race Condition

--- Test/test.c
+++ Test/test.c
@@ -6,7 +6,10 @@
 ///home/cc/gcc/ins/bin/gcc  -fplugin=/home/cc/gcc/myfile/Compiler/misra.so -I/home/cc/gcc/ins/lib/gcc/x86_64-pc-linux-gnu/7.3.0/plugin/include openssl_df_2.c  -O1  -flto  -fno-tree-dse  -fno-tree-fre -fno-tree-dce -fipa-pta   -fno-inline-functions-called-once   -o  openssl_df_2.o
 // 子執行緒函數
 pthread_mutex_t mLock;
-int *test;
+pthread_mutex_t mLock2;
+pthread_mutex_t mLock3;
+static	int *p99 ;
+static int fuck = 0;
 int *foo(int z)  __attribute__((noinline));
 int *foo2(int z) __attribute__((noinline));
 void foo3(int *z) __attribute__((noinline));
@@ -35,12 +38,18 @@ int *foo2(int z)
 	int *a2 = malloc(z);
 	int *p3 = malloc(z);
 	int *p4 = malloc(z);
+	p99 =malloc(z);
 	int tmp;
 	a2[0] = 10;
+	p99[0] = 10;
 	//  free(a2);
+	pthread_mutex_lock(&mLock);
+	pthread_mutex_lock(&mLock2);
+	pthread_mutex_lock(&mLock3);
+	foo3(p3);
 	if (tmp > 10)
 	{
-		pthread_mutex_unlock(&mLock);
+		// pthread_mutex_unlock(&mLock);
 		p3[0] = 10;
 		free(p3);
 		return p3;
@@ -168,6 +177,7 @@ void test22(int *k)
 	test22(k);
 	free(k);
 }
+
 int main()
 {
 	int *p;
@@ -181,6 +191,7 @@ int main()
 	p = foo2(2);
 	p[0] = 2;
 	p2 = foo2(2);
+		p2=&fuck;
 	p2[0] = 4;
 	// test22(p);
 	// test22(p);
@@ -189,22 +200,23 @@ int main()
 	free(p2);
 	char buff[50];
 	int *q = malloc(5);
-	q[0] = 10;
-	test22(q);
-	child((int)q);
-	int n;
-	pthread_t t; // 宣告 pthread 變數
-	pthread_mutex_destroy(&mLock);
-	pthread_create(&t, NULL, child, buff); // 建立子執行緒
 
-	// 主執行緒工作
-	//   for(int i = 0;i < 3;++i) {
-	//     printf("Master\n"); // 每秒輸出文字
-	//     sleep(1);
-	//   }
+	// q[0] = 10;
+	// test22(q);
+	// child((int)q);
+	// int n;
+	// pthread_t t; // 宣告 pthread 變數
+	// pthread_mutex_destroy(&mLock);
+	// pthread_create(&t, NULL, child, buff); // 建立子執行緒
 
-	pthread_join(t, NULL); // 等待子執行緒執行完成
-	pthread_mutex_destroy(&mLock);
+	// // 主執行緒工作
+	// //   for(int i = 0;i < 3;++i) {
+	// //     printf("Master\n"); // 每秒輸出文字
+	// //     sleep(1);
+	// //   }
+
+	// pthread_join(t, NULL); // 等待子執行緒執行完成
+	// pthread_mutex_destroy(&mLock);
 	// scanf("%d",n);
 	// p=foo(2);
 	// if(n)
